ea7a255ef3cd11d32722ab2618dde9bf9fc6bcf0
Victor Gonzalez Hermogenes <victorltrgh@gmail.com>
2022-08-20 14:40:23 -0300

    Aula terminada

--- 15_heranca_e_polimorfismo/1_heranca.py
+++ 15_heranca_e_polimorfismo/1_heranca.py
@@ -58,9 +58,54 @@ Quando uma classe herda de outra classe, a classe herdade é conhecida por:
     [Pessoa]
     -Super Classe;
     -Classe Mãe;
-    -Classe Pai
-    - Classe Base
-    -Classe Genérica
+    -Classe Pai;
+    - Classe Base;
+    -Classe Genérica.
+
+Quando uma classe herda de outra classe, ela é chamada:
+    [Cliente, Funcionario]
+    - Sub Classe;
+    - Classe Filha;
+    - Classe Específica.
+
+class Pessoa:
+    def __init__(self, nome, sobrenome, cpf):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
+
+class Cliente(Pessoa):
+    # Cliente herda de Pessoa
+
+    def __init__(self, nome, sobrenome, cpf, renda):
+        Pessoa.__init__(self, nome, sobrenome, cpf)    # Forma não comum de acessar dados da super classe
+        self.__renda = renda
+
+
+class Funcionario(Pessoa):
+    # Funcionário herda de pessoa
+
+    def __init__(self, nome, sobrenome, cpf, matricula):
+        super().__init__(nome, sobrenome, cpf)    # Forma comum de acessar dados da super classe
+        self.__matricula = matricula
+
+
+cliente1 = Cliente('Angelina', 'Jolie', '123.456.789-00', 5000)
+funcionario1 = Funcionario('Felicity', 'Jones', '987.654.321-11', 1234)
+
+print(cliente1.nome_completo())
+print(funcionario1.nome_completo())
+
+print(cliente1.__dict__)
+print(funcionario1.__dict__)
+
+# Sobrescrita de Métodos (Overriding)
+
+Sobrescrita de método, ocorre quando reescrevemos/reimplementamos um método presente na super classe
 """
 
 
@@ -75,21 +120,28 @@ class Pessoa:
 
 
 class Cliente(Pessoa):
-    """Cliente herda de Pessoa"""
+    # Cliente herda de Pessoa
+
     def __init__(self, nome, sobrenome, cpf, renda):
-        super().__init__(nome, sobrenome, cpf)
+        Pessoa.__init__(self, nome, sobrenome, cpf)    # Forma não comum de acessar dados da super classe
         self.__renda = renda
 
 
 class Funcionario(Pessoa):
-    """Funcionário herda de pessoa"""
+    # Funcionário herda de pessoa
+
     def __init__(self, nome, sobrenome, cpf, matricula):
-        super().__init__(nome, sobrenome, cpf)
+        super().__init__(nome, sobrenome, cpf)    # Forma comum de acessar dados da super classe
         self.__matricula = matricula
+
+    def nome_completo(self):
+        print(super().nome_completo())
+        print(self._Pessoa__cpf)
+        return f'Funcionário: {self.__matricula} Nome: {self._Pessoa__nome}'
 
 
 cliente1 = Cliente('Angelina', 'Jolie', '123.456.789-00', 5000)
 funcionario1 = Funcionario('Felicity', 'Jones', '987.654.321-11', 1234)
 
 print(cliente1.nome_completo())
-print(funcionario1.nome_competo())
+print(funcionario1.nome_completo())
