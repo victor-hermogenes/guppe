e89c14de0ca1048913298cddd3dc6f353e25207d
Victor Gonzalez Hermogenes <victorltrgh@gmail.com>
2022-08-20 15:29:31 -0300

    Modulo 15 em andamento, aula 1 terminada

---
+++ 15_heranca_e_polimorfismo/2_propriedades.py
@@ -0,0 +1,39 @@
+"""
+POO - Propriedades - Properties
+
+
+"""
+
+class Conta:
+
+    contador = 0
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta.contador + 1
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        Conta.contador += 1
+
+    def extrato(self):
+        return f'Saldo de {self.__saldo} do cliente {self.__titular}'
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.__saldo -= valor
+        destino.__saldo += valor
+
+
+conta1 = Conta('Felicity', 3000, 5000)
+conta2 = Conta('Angelina', 2000, 4000)
+
+print(conta1.extrato())
+print(conta2.extrato())
+
+soma = conta1._Conta__saldo + conta2._Conta__saldo
+print(f'A soma do saldo das contas é {soma}')
