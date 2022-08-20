80e89c74436c4ae8e194663fc76cd832547155d2
Victor Gonzalez Hermogenes <victorltrgh@gmail.com>
2022-08-20 16:01:46 -0300

    Aula 3 terminada

---
+++ 15_heranca_e_polimorfismo/03_super.py
@@ -0,0 +1,29 @@
+"""
+POO - O método super()
+
+O método super() se refere a super classe.
+
+"""
+
+
+class Animal:
+
+    def __init__(self, nome, especie):
+        self.__nome = nome
+        self.__especie = especie
+
+    def faz_som(self, som):
+        print(f'O {self.__nome} fala {som}')
+
+
+class Gato(Animal):
+
+    def __init__(self, nome, especie, raca):
+        # Animal.__init__(self, nome, especie)
+        super().__init__(nome, especie)
+        self.__raca = raca
+
+
+felix = Gato('Felix', 'Felino', 'Angatorá')
+
+felix.faz_som("miau")
