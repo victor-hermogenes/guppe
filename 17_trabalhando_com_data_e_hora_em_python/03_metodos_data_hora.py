4bfee71ece7843b860aaf750a31277d159bb63dc
victor-hermogenes <victorltrgh@gmail.com>
2022-08-21 19:15:03 -0300

    Aula terminada

--- 17_trabalhando_com_data_e_hora_em_python/03_metodos_data_hora.py
+++ 17_trabalhando_com_data_e_hora_em_python/03_metodos_data_hora.py
@@ -1,5 +1,148 @@
 """
 Métodos de Data e Hora
 
+# Com now() conseguimos especificar um timezone (Fuso Horário)
+agora = datetime.datetime.now()    # now == agora
+print(agora)
+print(type(agora))
+print(repr(agora))
 
+# Com today() não conseguimos especificar um timezone (Fuso Horário)
+hoje = datetime.datetime.today()    # today == hoje
+print(hoje)
+print(type(hoje))
+print(repr(hoje))
+
+# Mudanças ocorrendo à meia-noite - combine()
+
+manutencao = datetime.datetime.combine((datetime.datetime.now() + datetime.timedelta(days=1)), datetime.time())
+
+print(manutencao)
+print(type(manutencao))
+print(repr(manutencao))
+
+# Encontrar o dia da semana - weekday()
+
+# os dias da semana do método weekday() começam em 0, sendo 0 a segunda-feira
+
+# 0 - Segunda-feira (Monday)
+# 1 - Terça-feira (Tuesday)
+# 2 - Quarta-feira (Wednesday)
+# 3 - Quinta-feira (Thursday)
+# 5 - Sábado (Saturday)
+# 6 - Domingo (Sunday)
+
+manutencao = datetime.datetime.combine((datetime.datetime.now() + datetime.timedelta(days=1)), datetime.time())
+
+print(manutencao.weekday())
+
+aniversario = input('Qual a sua data de nascimento? dd/mm/yyyy ')
+
+aniversario = aniversario.split('/')
+
+aniversario = datetime.datetime(year=int(aniversario[2]), month=int(aniversario[1]), day=int(aniversario[0]))
+
+if aniversario.weekday() == 0:
+    print('Você nasceu em uma segunda')
+elif aniversario.weekday() == 1:
+    print(f'Você nasceu em uma terça')
+elif aniversario.weekday() == 2:
+    print(f'Você nasceu em uma quarta')
+elif aniversario.weekday() == 3:
+    print(f'Você nasceu em uma quinta')
+elif aniversario.weekday() == 4:
+    print(f'Vocẽ nasceu em uma sexta')
+elif aniversario.weekday() == 5:
+    print(f'Você nasceu em um sábado')
+elif aniversario.weekday() == 6:
+    print(f'Você nasceu em um domingo')
+
+# Formatando datas/horas com strftime() (String Format Time)
+# dd/mm/yyyy hora:minuto
+
+hoje = datetime.datetime.now()
+
+print(hoje)
+
+hoje_formatado = hoje.strftime('%d/%m/%Y')
+
+print(hoje_formatado)
+
+def formata_data(data):
+    if data.month == 1:
+        return f'{data.day} de Janeiro de {data.year}'
+    elif data.month == 2:
+        return f'{data.day} de Fevereiro de {data.year}'
+    elif data.month == 3:
+        return f'{data.day} de Março de {data.year}'
+    elif data.month == 4:
+        return f'{data.day} de Abril de {data.year}'
+    elif data.month == 5:
+        return f'{data.day} de Maio de {data.year}'
+    elif data.month == 6:
+        return f'{data.day} de Junho de {data.year}'
+    elif data.month == 7:
+        return f'{data.day} de Julho de {data.year}'
+    elif data.month == 8:
+        return f'{data.day} de Agosto de {data.year}'
+    elif data.month == 9:
+        return f'{data.day} de Setembro de {data.year}'
+    elif data.month == 10:
+        return f'{data.day} de Outubro de {data.year}'
+    elif data.month == 11:
+        return f'{data.day} de Novembro de {data.year}'
+    elif data.month == 12:
+        return f'{data.day} de Dezembro de {data.year}'
+
+from textblob import TextBlob
+
+
+def formata_data(data):
+    return f"{data.day} de {TextBlob(data.strftime('%B')).translate(to='pt-br')} de {data.year}"
+
+
+hoje = datetime.datetime.now()
+
+print(formata_data(hoje))
+
+nascimento = datetime.datetime.strptime('01/07/1997', '%d/%m/%Y')
+
+print(nascimento)
+
+nascimento = input('Qual a sua data de nascimento? (dd/mm/yyy): ')
+
+nascimento = datetime.datetime.strptime(nascimento, '%d/%m/%Y')
+
+print(nascimento)
+
+# Somente a hora
+
+almoco = datetime.time(12, 30, 0)
+print(almoco)
+
+# Marcando o tempo de execução do nosso código com timeit
+
+# Loop for
+tempo = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(tempo)
+
+# List Comprehension
+tempo = timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000)
+print(tempo)
+
+# Map
+tempo = timeit.timeit('"-".join(map(str, range(100)))', number=10000)
+print(tempo)
 """
+
+import timeit, functools
+
+
+def teste(n):
+    soma = 0
+    for num in range(n * 200):
+        soma = soma + num ** num + 4
+    return soma
+
+
+print(timeit.timeit(functools.partial(teste, 2), number=10000))
