4e90d5cc4d608850bbc12e6d464f2d808e81ceaa
victor-hermogenes <victorltrgh@gmail.com>
2022-08-20 21:34:26 -0300

    Modulo 16, Aula 1

---
+++ 16_manipulando_arquivos_csv_e_json/01_lendo_csv.py
@@ -0,0 +1,48 @@
+"""
+Lendo arquivos CSV
+
+CSV - Comma Separeted Values - Valores Separados por Vírgula
+
+# Separador por vírgula
+
+1, 2, 3, 4, 5, 6
+
+"geek", "university", "python", "ciência", "dados
+
+# Separador por ponto e vírgula
+
+1; 2; 3; 4; 5; 6
+
+"geek"; "university"; "python"; "ciência"; "dados
+
+# Separador por espaço
+
+1 2 3 4 5 6
+
+"geek" "university" "python" "ciência" "dados
+
+https://dados.gov.br/dataset
+
+# Possível de se trabalhar, mas não é o ideal (trabalhoso)
+
+with open('lutadores.csv') as arquivo:
+    dados = arquivo.read()
+    # print(type(dados))
+    dados = dados.split(',')[2:]
+    print(dados)
+
+A linguagem Python possui duas fdormas diferentes para ler dados em arquivos CSV:
+    - reador -> Permite que iteremos sobre as linhas do arquivo CSV como listas;
+    - DictReader -> Permite que iteremos sobre as linhas do arquivo CSV como OrderedDicts;
+"""
+
+# reader
+
+from csv import reader
+
+with open('lutadores.csv') as arquivo:
+    leitor_csv = reader(arquivo)
+    next(leitor_csv)    # Pular o cabeçalho
+    for linha in leitor_csv:
+        # Cada linha é uma lista
+        print(f'{linha[0]} nasceu no(a)(s) {linha[1]} e mede {linha[2]} centímetros')
