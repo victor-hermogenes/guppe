b65faf48a65c8f0073f28e5d0da9c716db986387
victor-hermogenes <victorltrgh@gmail.com>
2022-08-20 21:40:33 -0300

    Aula terminada

--- 16_manipulando_arquivos_csv_e_json/01_lendo_csv.py
+++ 16_manipulando_arquivos_csv_e_json/01_lendo_csv.py
@@ -34,7 +34,6 @@ with open('lutadores.csv') as arquivo:
 A linguagem Python possui duas fdormas diferentes para ler dados em arquivos CSV:
     - reador -> Permite que iteremos sobre as linhas do arquivo CSV como listas;
     - DictReader -> Permite que iteremos sobre as linhas do arquivo CSV como OrderedDicts;
-"""
 
 # reader
 
@@ -46,3 +45,24 @@ with open('lutadores.csv') as arquivo:
     for linha in leitor_csv:
         # Cada linha é uma lista
         print(f'{linha[0]} nasceu no(a)(s) {linha[1]} e mede {linha[2]} centímetros')
+
+# DictReader
+
+from csv import DictReader
+
+with open('lutadores.csv') as arquivo:
+    leitor_csv = DictReader(arquivo)
+    for linha in leitor_csv:
+        # Cada linha é um OrderedDict
+        print(f"{linha['Nome']} nasceu no(a)(s) {linha['País']} e mede {linha['Altura (em cm)']}")
+"""
+
+# DictReader
+
+from csv import DictReader
+
+with open('lutadores.csv') as arquivo:
+    leitor_csv = DictReader(arquivo, delimiter=',')
+    for linha in leitor_csv:
+        # Cada linha é um OrderedDict
+        print(f"{linha['Nome']} nasceu no(a)(s) {linha['País']} e mede {linha['Altura (em cm)']}")
