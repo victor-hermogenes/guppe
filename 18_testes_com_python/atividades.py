628a99fe4df00544b5c5bf1197a3029db8ec68c1
Victor-Main <victorltrgh@gmail.com>
2023-01-19 16:49:41 -0300

    Aula 18, módulo 4 terminados.

---
+++ 18_testes_com_python/atividades.py
@@ -0,0 +1,14 @@
+def comer(comida, e_saudavel):
+    if e_saudavel:
+        final = 'quero manter a forma.'
+    else:
+        final = 'a gente só vive uma vez.'
+
+    return f'Estou comendo {comida} porque {final}'
+
+
+def dormir(num_horas):
+    if num_horas > 8:
+        return f'Puts, dormi muito... Estou atrasado para o trabalho.'
+    else:
+        return f'Continuo cansado após dormir por {num_horas} horas. :('
