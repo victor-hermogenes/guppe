a6af20caba830eeb572f77fb0160603b8f79d6bf
Victor-Main <victorltrgh@gmail.com>
2023-01-23 18:51:25 -0300

    Aula 20, modulo 4 terminado.

---
+++ 20_checagem_de_tipos_em_python/04_checagem_de_tipos_mypy.py
@@ -0,0 +1,14 @@
+# def cabecalho(texto: str, alinhamento: bool = True) -> str:
+#     if alinhamento:
+#         return f'{texto.title()}\n{"-" * len(texto)}'
+#     else:
+#         return f' {texto.title()} '.center(50, '#')
+
+
+# print(cabecalho('Geek University'))
+
+# print(cabecalho('Geek University', alinhamento=False))
+
+# print(cabecalho('Geek University', alinhamento=True))
+
+# Usa-se escrevendo mypy nome_do_arquivo.py
