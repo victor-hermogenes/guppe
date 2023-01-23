f74e424bd032b58d5533efa6b43c2a81749f93d0
Victor-Main <victorltrgh@gmail.com>
2023-01-23 19:25:56 -0300

    Aula 20, modulo 6 terminado.

---
+++ 20_checagem_de_tipos_em_python/06_tipos_em_comentarios.py
@@ -0,0 +1,43 @@
+import math
+
+
+# Certo
+def circunferencia(raio):
+    # type: (float) -> float
+    return 2 * math.pi * raio
+
+
+# Errado -> Duplicado
+# def circunferencia(raio: float) -> float:
+#     # type: (float) -> float
+#     return 2 * math.pi * raio
+
+
+print(circunferencia(8))
+# print(circunferencia('geek'))
+
+
+def cabecalho1(texto, alinhamento=True):
+    # type: (str, bool) -> str
+    if alinhamento:
+        return 'a'
+    else:
+        return 'b'
+
+
+# cabecalho1(texto=43, alinhamento='geek')
+
+
+def cabecalho2(
+        texto, # type: str
+        alinhamento=True # type: bool
+):  # type: (...) -> str
+    if alinhamento:
+        return 'a'
+    else:
+        return 'b'
+
+
+cabecalho2(texto='42', alinhamento=False)
+
+nome = 'Geek University'
