1f9f79d13c0ec2edb2c278d92a888d30052c5ccf
Victor-Main <victorltrgh@gmail.com>
2023-01-30 10:34:03 -0300

    Projeto: Testes finalizados, projeto concluído.

--- GamePy/models/calcular.py
+++ GamePy/models/calcular.py
@@ -81,7 +81,7 @@ class Calcular:
             certo = True
         else:
             print('Resposta errada!')
-        print(f'{self.valor1} {self._op_simboolo} {self.valor2} = {self.resultado}')
+        print(f'{self.valor1} {self._op_simbolo} {self.valor2} = {self.resultado}')
         return certo
 
     def mostrar_operacao(self: object) -> None:
